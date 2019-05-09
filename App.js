123129efe4724763c988d6bf9a41f8c076ca8506
DESKTOP-UEOV98L\Rodolfo <rodolfoneto@gmail.com>
2019-05-09 13:38:03 -0300

    Class about Network

--- App.js
+++ App.js
@@ -1,52 +1,56 @@
 import React, {Component} from 'react';
 import {
-  View,
-  StyleSheet,
-  FlatList,
-  Text,
-  SectionList,
+  View,FlatList, ActivityIndicator,Text
 } from 'react-native';
 
-class SectionListBasics extends Component {
-  render() {
-    return (
-      <SectionList
-        sections={[
-          {title: 'D', data: ['Devin']},
-          {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-        renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-        keyExtractor={(item, index) => index}
-      />
+class FetchExample extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+  render(){
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
     );
-  };
+  }
 }
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   paddingTop: 22
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-})
+
+
 export default class SecondClass extends Component {
   render() {
     return (
       <View style={{padding: 10, backgroundColor: 'white', flex:1}} >
-        <SectionListBasics />
+        <FetchExample />
       </View>
     );
   }
