465558590873d65e7d127cb263e7cf6695a9e6ee
DESKTOP-UEOV98L\Rodolfo <rodolfoneto@gmail.com>
2019-05-09 13:12:09 -0300

    Class for FlatList

--- App.js
+++ App.js
@@ -1,140 +1,47 @@
 import React, {Component} from 'react';
 import {
-  Text, 
-  TextInput, 
-  View, 
-  Button, 
-  Alert, 
-  TouchableNativeFeedback, 
-  StyleSheet, 
-  ScrollView, 
-  Image,
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
 } from 'react-native';
 
-class ItemRender extends Component {
-  render() {
-    return (
-      <View style={{backgroundColor: 'purple', height: 110, width:220, paddingBottom: 10, flexDirection: 'row' }}>
-        <View style={{backgroundColor: 'blue', width: 100, height: 100, marginRight: 10}}/>
-        <View style={{backgroundColor: 'red', width: 100, height: 100}}/>
-      </View>
-    );
-  }
-}
-
-class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
-  render() {
-    return (
-      <ScrollView>
-        <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>If you like</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>Scrolling down</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>What's the best</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Text style={{fontSize:96}}>Framework around?</Text>
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-          <Text style={{fontSize:80}}>React Native</Text>
-      </ScrollView>
-    );
-  }
-}
-
-class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text:''};
-  }
-
-  render() {
-    return (
-      <View style={{padding: 10}} >
-        <TextInput style={{height: 40}} placeholder="Type here to translate"
-          onChangeText={(text) => { this.setState({text})}}/>
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-      </View>
-    );
-  }
-}
-
-class FirstButton extends Component {
+class FlatListBasics extends Component {
   render() {
     return(
-      <Button
-        onPress={()=>{
-          Alert.alert('OK! Now Stop.')
-        }}
-        title="Press Me" />
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
     );
-  }
-}
-
-class Touchable extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
-
-  _onLongPressButton() {
-    Alert.alert('You long-pressed the button!')
-  }
-  render() {
-    return (
-      <TouchableNativeFeedback
-        onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor='white'>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Touchable with Long Press</Text>
-        </View>
-      </TouchableNativeFeedback>
-    );
-  }
+  };
 }
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
-    alignItems: 'center'
+   flex: 1,
+   paddingTop: 22
   },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  }
-});
+})
 
-
-export default class FirstProj extends Component {
+export default class SecondClass extends Component {
   render() {
     return (
-      <View style={{padding: 10, backgroundColor: 'gray', flex:1}} >
-        <IScrolledDownAndWhatHappenedNextShockedMe />
+      <View style={{padding: 10, backgroundColor: 'white', flex:1}} >
+        <FlatListBasics />
       </View>
     );
   }
